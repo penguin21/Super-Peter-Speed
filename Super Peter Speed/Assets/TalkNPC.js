e2a710bb9b1765494a3d6bd2c59927a1416c85df
penguin21 <pepesanchezjose68@gmail.com>
2014-09-01 17:21:41 +0200

    New Files

--- Super Peter Speed/Assets/TalkNPC.js
+++ Super Peter Speed/Assets/TalkNPC.js
@@ -1,9 +1,35 @@
-﻿#pragma strict
+﻿var TalkText : GUIText;
+var talkLines : String[];
+var MessageObjectLabel : GameObject;
 
-function Start () {
+private var Talking : boolean = false;
+private var CurrentLine : int;
 
+function OnTriggerStay2D(other : Collider2D){
+	if(other.gameObject.tag == "Player"){
+		if(Input.GetButton("Interact")){
+			MessageObjectLabel.SetActive(true);
+			Talking = true;
+			CurrentLine = 0;
+			TalkText.text = talkLines[CurrentLine];
+		}
+	}
 }
 
-function Update () {
-
+function Update(){
+	if(Talking){
+		if(Input.GetButton("Interact")){
+		//Next Text
+		if(CurrentLine < talkLines.Length - 1){
+		CurrentLine++;
+		TalkText.text = talkLines[CurrentLine];
+		}
+		else
+		{
+			CurrentLine = 0;
+			TalkText.text = "";
+			Talking = false;
+			}
+		}
+	}
 }
