69acb37eb70ca59c272c436c8803724641a48aaf
penguin21 <pepesanchezjose68@gmail.com>
2014-09-17 16:18:56 +0200

    Files

--- Super Peter Speed/Assets/Scripts/AI/AIChase.js
+++ Super Peter Speed/Assets/Scripts/AI/AIChase.js
@@ -1,55 +1,38 @@
-﻿var target : Transform; 
-var moveSpeed = 3; 
-var rotationSpeed = 3; 
-var attackThreshold = 3; 
-var chaseThreshold = 10; 
-var maxDistance = 3; 
-var giveUpThreshold = 20; 
-var attackRepeatTime = 1;
+﻿var PlayerTarget : Transform;
+var L : boolean = false;
+var R : boolean = false;
+var size : int = 7;
+var IsFollowing : boolean = false;
+var speed : float = 5;
+var searhRange : float = 15;
 
-private var chasing = false; 
-private var attackTime = Time.time;
+private var distanceToTarget : float = 0.0;
 
-var myTransform : Transform;
-
-function Awake() { 
-myTransform = transform; 
+function Update(){
+	distanceToTarget = Vector3.Distance(PlayerTarget.transform.position, transform.position);
+	if(distanceToTarget <= searhRange){
+		ChasePlayer();
+	}
+	
+	if(L == true){
+		transform.localScale.x = size;
+		transform.Translate(Vector3(speed,0,0) * Time.deltaTime);
+	}
+	
+	if(R == true){
+		transform.localScale.x = -size;
+		transform.Translate(Vector3(-speed,0,0) * Time.deltaTime);
+		}
 }
 
-function Start() { 
-target = GameObject.FindWithTag("Player").transform; //target the player }
-}
-function Update () {
-
-// check distance to target every frame:
-var distance = (target.position - myTransform.position).magnitude;
- 
-if (chasing) {
- 
-    //rotate to look at the player
-    myTransform.rotation = Quaternion.Slerp(myTransform.rotation,
-    Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
- 
-    //move towards the player
-    myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
- 
-    // give up, if too far away from target:
-    if (distance > giveUpThreshold) {
-        chasing = false;
-    }
- 
-    // attack, if close enough, and if time is OK:
-    if (distance < attackThreshold && Time.time > attackTime) {
-        // Attack! (call whatever attack function you like here)
-        attackTime = Time.time + attackRepeatTime;
-    }
- 
-} else {
-    // not currently chasing.
- 
-    // start chasing if target comes close enough
-    if (distance < chaseThreshold) {
-        chasing = true;
-    }
-}
+function ChasePlayer(){
+	if(transform.position.x <= PlayerTarget.position.x){
+		L = true;
+		R = false;
+	}
+	
+	if(transform.position.x >= PlayerTarget.position.x){
+		L = false;
+		R = true;
+	}
 }
