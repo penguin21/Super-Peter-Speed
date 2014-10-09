d2140b1624f3d38bab5df593d884846fa7a6530d
penguin21 <pepesanchezjose68@gmail.com>
2014-10-09 22:06:05 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/Pause.js
@@ -0,0 +1,26 @@
+﻿var IsPause : boolean = false;
+var Menu : boolean = false;
+
+function Start () {
+IsPause = false;
+}
+
+function Update () {
+
+	if(IsPause == true){
+		Time.timeScale = 0.0;
+		IsPause = true;
+		Menu = true;
+	}
+	
+	if(IsPause == false){
+		Time.timeScale = 1.0;
+		IsPause = false;
+		Menu = false;
+	}
+}
+
+function OnGUI(){
+
+
+}
