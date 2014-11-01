605d75fb29c5ad18687ad38dc2e4dfa4f84fbaee
penguin21 <pepesanchezjose68@gmail.com>
2014-11-01 13:21:05 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/AI/ObjFloatingOnWater.js
@@ -0,0 +1,25 @@
+﻿var SpeedObject : float = 1;
+var FloatIs : boolean = false;
+var Obje : Rigidbody2D;
+
+function Start () {
+
+}
+
+function Update () {
+	if(FloatIs == true){
+		Obje.AddForce(transform.up * SpeedObject);
+	}
+}
+
+function OnTriggerStay2D (other : Collider2D){
+	if(other.gameObject.tag == "Water"){
+		FloatIs = true;
+	}
+}
+
+function OnTriggerExit2D (other : Collider2D){
+	if(other.gameObject.tag == "Water"){
+		FloatIs = false;
+	}
+}
