9920a50f4c0fb7b735507399a5757306f88da293
penguin21 <pepesanchezjose68@gmail.com>
2014-11-03 15:46:58 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/DoorTransform.js
@@ -0,0 +1,17 @@
+﻿var DoorTrans : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerStay2D(other : Collider2D){
+	if(other.gameObject.tag == "Player"){	
+		if(Input.GetButton("Interact")){
+			other.transform.position = DoorTrans.transform.position;
+		}
+	}
+}
---
+++ Super Peter Speed/Assets/Scripts/TilingBG.js
@@ -0,0 +1,31 @@
+﻿ #pragma strict
+ import System.Collections.Generic;
+ 
+ var mapSizeX : int;
+ var mapSizeY : int;
+ 
+ var tilePrefab : GameObject;
+ 
+ 
+ var tiles : List.<Transform> = new List.<Transform>(); 
+ 
+ 
+ function Start () {
+     var i : int = 0;
+     var xIndex : int = 0;
+     var yIndex : int = 0;
+     
+     while(yIndex < mapSizeY){
+         xIndex = 0;
+         while(xIndex < mapSizeX){
+             var newTile : GameObject = Instantiate (tilePrefab, Vector3(xIndex*0.32, yIndex*0.32, 0), Quaternion.identity);
+             tiles.Add(newTile.transform);
+             newTile.transform.parent = transform;
+             newTile.transform.name = "tile_"+i;
+             i++;
+             xIndex++;
+         }
+     
+         yIndex++;
+     }
+ }
