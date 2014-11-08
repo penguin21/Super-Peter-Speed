e4af88778a9bbec8cfd35018e2905babee1fd9eb
penguin21 <pepesanchezjose68@gmail.com>
2014-11-08 21:26:59 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Player/GetItemGun.js
@@ -0,0 +1,28 @@
+﻿var GunItem : GameObject;
+var ArmIsNotActive : boolean = false;
+var Arm : GameObject;
+var NameSaveGun : String;
+var SoundGet : AudioClip;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionStay2D(other: Collision2D) {
+	if(other.gameObject.tag == "Player"){
+		if(ArmIsNotActive == true){
+			audio.PlayOneShot(SoundGet, 0.7);
+			Arm.SetActive(true);
+			Destroy(gameObject);
+		}
+		if(ArmIsNotActive == false){
+			audio.PlayOneShot(SoundGet, 0.7);
+			GunItem.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
+}		
