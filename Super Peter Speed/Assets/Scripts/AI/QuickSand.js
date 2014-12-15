c5d8aa99f8801d473a0b21f6d577ef24ebaed6a5
penguin21 <pepesanchezjose68@gmail.com>
2014-12-15 16:42:29 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/AI/QuickSand.js
@@ -0,0 +1,36 @@
+﻿//var SandSpeed : float = 5;
+//var intensityForSand : float = 2;
+var GravityForSand = 0.2;
+private var DebugGravity = false;
+private var DebugGravityExit = false;
+
+function Start () {
+
+}
+
+function Update () {
+	if(DebugGravity == true){
+		Debug.Log("Is QuickSand");
+	}
+	if(DebugGravityExit == true){
+		Debug.Log("Is Exit QuickSand");
+	}
+}
+
+function OnTriggerStay2D(col : Collider2D){
+	if(col.gameObject.rigidbody2D !=null){
+		//var target : Vector2 = col.gameObject.transform.position;
+		//var sand : Vector2 = gameObject.transform.position;
+		
+		//var direction : Vector2 = SandSpeed * (target + sand);
+		
+		//col.gameObject.rigidbody2D.AddForce(new Vector2 (direction.x / 0, direction.y * intensityForSand));
+		col.rigidbody2D.AddForce(Vector2.up * GravityForSand);
+	}
+}
+
+function OnTriggerExit2D(col : Collider2D){
+	if(col.gameObject.rigidbody2D !=null){
+		//col.rigidbody2D.gravityScale = 1;
+	}
+}
