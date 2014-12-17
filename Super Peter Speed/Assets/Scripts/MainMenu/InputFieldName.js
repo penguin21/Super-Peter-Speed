4e06ed7a96377abc66ee9a771263120c910b3af1
penguin21 <pepesanchezjose68@gmail.com>
2014-12-17 16:42:41 +0100

    Files

--- Super Peter Speed/Assets/Scripts/MainMenu/InputFieldName.js
+++ Super Peter Speed/Assets/Scripts/MainMenu/InputFieldName.js
@@ -15,12 +15,18 @@ function Update () {
 	PlayerName = inputField.text;
 	nameDisplay.text = PlayerName;
 	if(Count == 1){
+		IsNamePanel.SetActive(true);
 		Panel.SetActive(false);
+	}
+	if(Count == 0){
+		IsNamePanel.SetActive(false);
+		Panel.SetActive(true);
 	}
 }
 
 function NameAccept(){
 	PlayerPrefs.SetString("PlayerName", PlayerName);
 	PlayerPrefs.SetInt("IsNameNo", 1);
+	IsNamePanel.SetActive(true);
 	Panel.SetActive(false);
 }
