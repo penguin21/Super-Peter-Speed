a9c6b7de29cdfedacaa65cde4fe40a06a85cf4c8
penguin21 <pepesanchezjose68@gmail.com>
2014-12-17 21:08:42 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/ChangeSceneToSecounds.js
@@ -0,0 +1,11 @@
+﻿var Wait : float = 5;
+var SceneToChange : String;
+
+function Update () {
+	ChangeScene(); //To changing
+}
+
+function ChangeScene(){
+	yield WaitForSeconds(Wait);
+	Application.LoadLevel(SceneToChange);
+}
