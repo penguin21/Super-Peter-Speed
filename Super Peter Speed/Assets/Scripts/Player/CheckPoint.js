4865c71cde5fb08a5c150ee77402c5a9c8274cd1
penguin21 <pepesanchezjose68@gmail.com>
2014-12-23 18:41:51 +0100

    Files

--- Super Peter Speed/Assets/Scripts/Player/CheckPoint.js
+++ Super Peter Speed/Assets/Scripts/Player/CheckPoint.js
@@ -1,11 +1,12 @@
 ﻿@script RequireComponent(AudioSource)
 var CheckPointSound : AudioClip;
 var CheckPoint : Transform;
+var FlagPoint : GameObject;
 
 private var checkpointIs = false;
 
 function Start () {
-
+	FlagPoint.SetActive(false);
 }
 
 function Update () {
@@ -17,6 +18,7 @@ function OnTriggerEnter2D(other : Collider2D){
 	if(other.gameObject.tag == "Player"){
 		other.GetComponent(PlayerAI).Respawn = CheckPoint;
 		audio.PlayOneShot(CheckPointSound, 0.7);
+		FlagPoint.SetActive(true);
 		Debug.Log("CheckPoint!!");
 		checkpointIs = true;
 		}
