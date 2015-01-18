0ce6b3a33520fee906a7ac2c78cb7011f4e4e375
penguin21 <pepesanchezjose68@gmail.com>
2015-01-18 18:22:00 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/FoldersData.js
@@ -0,0 +1,34 @@
+﻿var MusicFolder : String = "/Music";
+var LevelsFolder : String = "/Levels";
+var CustomSpritesFolder : String = "/CustomSprites";
+var LocalFolder : String = "/Super Peter Speed";
+var appdata : String;
+appdata = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)+ LocalFolder;
+
+function Awake(){
+
+}
+
+function Start () {
+		if(!Directory.Exists(appdata + MusicFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + MusicFolder);
+     Debug.Log("Directory Music is Created on " + appdata + MusicFolder);
+ 	}
+ 	
+ 	if(!Directory.Exists(appdata + LevelsFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + LevelsFolder);
+     Debug.Log("Directory Levels is Created on " + appdata + LevelsFolder);
+ 	}
+ 	
+ 	 if(!Directory.Exists(appdata + CustomSpritesFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + CustomSpritesFolder);
+     Debug.Log("Directory Custom Sprites is created on "+ appdata + CustomSpritesFolder);
+ 	}
+}
+
+function Update () {
+
+}
