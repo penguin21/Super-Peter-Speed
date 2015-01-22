75862c560eec5fcaaa46089cdd4e8d39da895cde
penguin21 <pepesanchezjose68@gmail.com>
2015-01-22 17:24:04 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/PeterPrison.js
@@ -0,0 +1,17 @@
+﻿var Sprite1 : Sprite;
+var Sprite2 : Sprite;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(other: Collider2D) {
+	Debug.Log("Is Enter");
+	other.collider2D.enabled = false;
+	yield WaitForSeconds(5);
+	gameObject.GetComponent(SpriteRenderer).sprite = Sprite1;
+}
