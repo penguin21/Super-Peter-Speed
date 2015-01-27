a2925a4caaec2f0db7d1188733e8cde3a2169aa8
penguin21 <pepesanchezjose68@gmail.com>
2015-01-27 16:32:19 +0100

    Files

---
+++ Super Peter Speed/Assets/OpenFileDalog/OpenFileDialog.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenFileDialog : MonoBehaviour {
+
+	public static OpenFileDialog instance;
+	private string path="";
+
+
+	bool isTextureLoaded = false;
+	bool isErrorUploading = false;
+	bool isCanceled = false;
+	bool isPathReturned = false;
+
+	private  Texture2D texture;
+
+	public Texture2D GetTexure()
+	{
+		return texture;
+	}
+
+
+	public void ShowDialog()
+	{
+		Application.ExternalEval("ShowDialog()");
+		debug += "showing dialog \n";
+	}
+
+
+
+
+
+	public void HideDialog()
+	{
+		Application.ExternalEval("HideDialog()");
+		debug += "hiding dialog \n";
+	}
+
+
+
+
+
+
+	
+
+private IEnumerator DownloadImage(string url)
+	{
+		WWW www = new WWW(url);
+		// wait until the download is done
+		debug+="downloading image from url =" +url +"\n";
+		yield return www;
+		// assign the downloaded image to the main texture of the object
+		texture = www.texture;
+	}
+
+	
+
+	public void FromJSCall_returnedAnswer(string answerCode)
+
+	{
+		debug+="returned answer = " + answerCode + "\n";
+		// code error - func onError
+		// code cancel -func onCancell
+		// code ok - func return path
+		if (answerCode == "error")
+		{
+			isErrorUploading = true;
+		}
+		if (answerCode == "cancel")
+		{
+			isCanceled = true;
+		}
+		if (answerCode == "succes")
+		{
+
+		//	path = answerText;
+
+			// js, send mi path pleace
+			JSsendMiiPAthPlease();
+
+		}
+		isAnswered = true;
+	}
+
+	public void FromJSCall_sendPath(string serverPathTophoto)
+	{
+		debug+="geted path is "+ serverPathTophoto + "\n";
+		path = serverPathTophoto;
+		isPathReturned = true;
+	}
+
+	private void JSsendMiiPAthPlease()
+	{
+		Application.ExternalEval("SendPathToUnity()");
+		debug += "JSsendMiiPAthPlease :\n";
+	}
+
+
+	private bool isAnswered = false;
+	public IEnumerator GetPhotoCoroutines()
+	{
+		ResetAllBoolians();
+	
+		// 1 openfile dialog // wite while close or ok
+		ShowDialog();
+		// yield return url or cancell or error
+		while (isAnswered == false) {
+			// in bowser user open image if apoloaded or no - reqwestetd to FromJSCall_returnedAnswer
+			// we wait isAnswered true that function continue
+			// after answer js sen to unity path to file and isPathReturned = true
+			// after succes downloading file
+			Debug.Log ("wait foe anwer...\n");
+			debug+=".";
+			yield return null;		
+		}
+
+		while (isPathReturned == false) {
+			if (isErrorUploading == true || isCanceled == true)
+			{ break;}
+
+			Debug.Log ("waity foe path...\n");
+			debug+="/";
+			yield return null;		
+		}
+		if(isPathReturned)
+		{
+			yield return StartCoroutine(DownloadImage(path));
+		}
+		else
+		{
+			yield return 0;
+	
+		}
+	}
+
+
+
+	private void ResetAllBoolians()
+	{
+		isTextureLoaded = false;
+		isErrorUploading = false;
+		isCanceled = false;
+		isPathReturned = false;
+		isAnswered = false;
+		path = "";
+		texture = null;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		debug+="In play mode not work (uses jquery in webplayer) by default is showing debug,\n if you wnt disable debug - isShowDebug in inspector\n";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public string debug;
+	public bool isShowDebug = true;
+
+	void OnGUI()
+	{
+		if (isShowDebug)
+		{
+		GUI.Label(new Rect(0,0,500,800),debug);
+		GUI.Box(new Rect(300,0,100,100),texture);
+		}
+	}
+}
---
+++ Super Peter Speed/Assets/OpenFileDalog/sample.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sample : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+
+	private Texture2D photo1;
+IEnumerator LoadPhoto1()
+{
+		yield return StartCoroutine( OpenFileDialog.instance.GetPhotoCoroutines());
+		photo1 = OpenFileDialog.instance.GetTexure();
+
+}
+
+
+
+	private Texture2D photo2;
+	IEnumerator LoadPhoto2()
+	{
+		yield return StartCoroutine( OpenFileDialog.instance.GetPhotoCoroutines());
+		photo2 = OpenFileDialog.instance.GetTexure();
+		
+	}
+
+void	OnGUI()
+	{
+
+		if (GUI.Button(new Rect(100,100,300,20), "load photo 1"))
+		{
+			StartCoroutine (LoadPhoto1());
+		}
+
+
+		if (GUI.Button(new Rect(100,140,300,20), "load photo 2"))
+		{
+			StartCoroutine (LoadPhoto2());
+		}
+
+		GUI.Box(new Rect( 100,0,100,100), photo1);
+		GUI.Box(new Rect( 200,0,100,100), photo2);
+
+	}
+}
