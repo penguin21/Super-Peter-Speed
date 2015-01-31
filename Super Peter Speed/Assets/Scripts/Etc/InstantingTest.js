324ab946f34b307c69bb9e62f06670fca9fefcb1
penguin21 <pepesanchezjose68@gmail.com>
2015-01-31 15:55:44 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/InstantingTest.js
@@ -0,0 +1,22 @@
+﻿var TestObj : GameObject;
+var TestSpawn :GameObject;
+var Screen1 = 20;
+
+function Start () {
+
+}
+
+ function Update () 
+     {
+     if (Input.GetMouseButtonDown(0)) 
+         {
+ 		 var pos = Input.mousePosition;
+         pos.z = transform.position.z;
+         pos = Camera.main.ScreenToWorldPoint(pos);
+         
+         var q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+         var go = Instantiate(TestObj, transform.position, q);
+         go.transform.localPosition = Vector3(0, 0, 5);
+         go.transform.localRotation = Quaternion.identity;
+         }
+     }
