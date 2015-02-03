4059b76af047356a291a2a19e0c71f67d7af0399
penguin21 <pepesanchezjose68@gmail.com>
2015-02-03 16:22:24 +0100

    Files

--- Super Peter Speed/Assets/Scripts/AI/FallPlatform.js
+++ Super Peter Speed/Assets/Scripts/AI/FallPlatform.js
@@ -1,8 +1,19 @@
 ﻿var timeToFall : float;
+var TimeToRespawn = 10.0;
+var RespawnPoint : Transform;
 
 function OnCollisionEnter2D(other : Collision2D){
 	if(other.gameObject.tag == "Player"){
 	yield WaitForSeconds(timeToFall);
 	rigidbody2D.isKinematic = false;
+	yield WaitForSeconds(TimeToRespawn);
+	rigidbody2D.isKinematic = true;
+	transform.localRotation.z = 0;
+	if(!RespawnPoint){
+			//Is platforms no error
+		}
+		else{
+			gameObject.transform.position = RespawnPoint.transform.position;
+		}
 	}
 }
