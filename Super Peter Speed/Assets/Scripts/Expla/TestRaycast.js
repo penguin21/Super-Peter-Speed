bd3cfe1c20ce9a6ecc0936a8bfb8a7a102930907
penguin21 <pepesanchezjose68@gmail.com>
2015-02-10 16:52:59 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Expla/TestRaycast.js
@@ -0,0 +1,20 @@
+﻿var Col1 : Transform;
+var Col2 : Transform;
+var CollsionIs = false;
+
+function Start () {
+
+}
+
+function Update () {
+	RayCasting();
+	Ber();
+}
+
+function RayCasting(){
+	CollsionIs = Physics2D.Linecast(Col1.position, Col2.position);
+}
+
+function Ber (){
+	
+}
