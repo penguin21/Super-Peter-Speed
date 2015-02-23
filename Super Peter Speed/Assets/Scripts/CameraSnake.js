2dc057430a7f40b4cc5b15937a6a17ebf8e7b26c
penguin21 <pepesanchezjose68@gmail.com>
2015-02-23 22:05:24 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/CameraSnake.js
@@ -0,0 +1,27 @@
+﻿var originPosition:Vector3;
+var originRotation:Quaternion;
+var shake_decay: float;
+var shake_intensity: float;;
+var ShakeIs = false;
+ 
+function Update(){
+   if(shake_intensity > 0){
+      transform.position = originPosition + Random.insideUnitSphere * shake_intensity;
+      transform.rotation = Quaternion(
+      originRotation.x + Random.Range(-shake_intensity,shake_intensity)*.2,
+      originRotation.y + Random.Range(-shake_intensity,shake_intensity)*.2,
+      originRotation.z + Random.Range(-shake_intensity,shake_intensity)*.2,
+      originRotation.w + Random.Range(-shake_intensity,shake_intensity)*.2);
+      shake_intensity -= shake_decay;
+   }
+   if(ShakeIs == true){
+   		Shake();
+   }
+}
+ 
+function Shake(){
+   originPosition = transform.position;
+   originRotation = transform.rotation;
+   shake_intensity = .3;
+   shake_decay = 0.002;
+}
