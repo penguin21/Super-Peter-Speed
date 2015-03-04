8bcc58e143737182f78712f92bcb59b34af53000
penguin21 <pepesanchezjose68@gmail.com>
2015-03-04 16:55:49 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/CreditsCamera.js
@@ -0,0 +1,13 @@
+﻿var SpeedCam : float = 5;
+var Secounds : float = 125;
+var SceneToChange : String;
+
+function Update () {
+	CameraCredits();
+}
+
+function CameraCredits(){
+	transform.Translate(Vector3(0,-SpeedCam,0) * Time.deltaTime);
+	yield WaitForSeconds(Secounds);
+	Application.LoadLevel(SceneToChange);
+}
