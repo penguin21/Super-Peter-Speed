a3ccd4a32e7c509c96096cebfa298907a5b17513
penguin21 <pepesanchezjose68@gmail.com>
2015-03-07 13:29:49 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/AI/Bosses/GiantDarkPeter/DPBAI.js
@@ -0,0 +1,13 @@
+﻿var BossActive = true;
+var speed = 10.0;
+var speedX = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	if(BossActive == true){
+		transform.Translate(Vector3(-speed,0,speedX) * Time.deltaTime);
+	}
+}
