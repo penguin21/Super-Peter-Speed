bfd216b94e3eff46270173b155158b0daebf1157
penguin21 <pepesanchezjose68@gmail.com>
2015-03-25 16:43:27 +0100

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/WinEvent.js
@@ -0,0 +1,22 @@
+﻿var MusicWin : AudioClip;
+var CameraObj : GameObject;
+
+function Start () {
+	CameraObj = GameObject.FindGameObjectWithTag("MainCamera");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(other : Collider2D){
+	if(other.gameObject.tag == "Player"){
+		WinLevel();
+	}
+}
+
+function WinLevel(){
+	CameraObj.GetComponent.<AudioSource>().clip = MusicWin;
+	CameraObj.GetComponent.<AudioSource>().loop = false;
+	CameraObj.GetComponent.<AudioSource>().Play();
+}
