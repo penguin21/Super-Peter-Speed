cd8a3961ca6a8660aa7bec395b98c89e2cfe3692
penguin21 <pepesanchezjose68@gmail.com>
2015-03-30 21:59:55 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/LeanManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Lean;
+
+public class LeanManager : MonoBehaviour {
+
+	public int LangID = 0;
+
+	// Use this for initialization
+	void Start () {
+		LangID = PlayerPrefs.GetInt("Language");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
