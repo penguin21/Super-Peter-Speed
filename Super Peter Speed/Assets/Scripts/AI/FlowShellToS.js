441a516aca868ea761906ad1d5bf356c958c8b1e
penguin21 <pepesanchezjose68@gmail.com>
2015-04-10 21:57:38 +0200

    Files

--- Super Peter Speed/Assets/Scripts/AI/FlowShellToS.js
+++ Super Peter Speed/Assets/Scripts/AI/FlowShellToS.js
@@ -3,12 +3,13 @@ var EnemyStompSound : AudioClip;
 var EnemyStompObj : GameObject;
 var ShellInstantSpawn : Transform;
 var Shell : GameObject;
+var AudioStomp : GameObject;
 
 function OnCollisionEnter2D (other : Collision2D){
 	if(other.gameObject.tag == "Player"){
-		AudioSource.PlayClipAtPoint(EnemyStompSound, transform.position);
 		EnemyObj.tag = "Untagged";
 		Instantiate(Shell, ShellInstantSpawn.transform.position, ShellInstantSpawn.transform.rotation);
+		Instantiate(AudioStomp, ShellInstantSpawn.transform.position, ShellInstantSpawn.transform.rotation);
 		Destroy(EnemyObj);
 	}
 }
