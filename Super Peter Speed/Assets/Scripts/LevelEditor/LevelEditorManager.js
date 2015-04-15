3120b87b3b072af1258232b128225b35728b95cf
penguin21 <pepesanchezjose68@gmail.com>
2015-04-15 17:00:14 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorManager.js
@@ -0,0 +1,15 @@
+﻿var LevelT : Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function DeleteAllObjects(){
+	 for (var child : Transform in LevelT){
+     GameObject.Destroy(child.gameObject);
+ 	 }	
+}
