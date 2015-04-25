4f28dd4320f9c09e0d18c2248d9e31346a7979bf
penguin21 <pepesanchezjose68@gmail.com>
2015-04-25 14:34:38 +0200

    Files

--- Super Peter Speed/Assets/Scripts/CameraBounds.js
+++ Super Peter Speed/Assets/Scripts/CameraBounds.js
@@ -1,26 +1,27 @@
-﻿ var mapX = 100.0;
-     var mapY = 100.0;
-     
-     var minX : float;
-     var maxX : float;
-     var minY : float;
-      var maxY : float;
-     
-     
-     function Start() {
-         var vertExtent = Camera.main.GetComponent.<Camera>().orthographicSize;    
-         var horzExtent = Camera.main.ortiographicSize * Screen.width / Screen.height;
+﻿var mapX = 100.0; //do not use
+var mapY = 100.0; //do not use
+var minX : float;
+var maxX : float;
+var minY : float;
+var maxY : float;     
+
+function Start() {
+//Old is a error on play
+//var vertExtent = Camera.main.GetComponent.<Camera>().orthographicSize;    
+//var horzExtent = Camera.main.ortiographicSize * Screen.width / Screen.height;
  
-         // Calculations assume map is position at the origin
-         minX = horzExtent - mapX / 2.0;
-         maxX = mapX / 2.0 - horzExtent;
-         minY = vertExtent - mapY / 2.0;
-         maxY = mapY / 2.0 - vertExtent;
-     }
+// Calculations assume map is position at the origin
+//minX = horzExtent - mapX / 2.0;
+//maxX = mapX / 2.0 - horzExtent;
+//minY = vertExtent - mapY / 2.0;
+//maxY = mapY / 2.0 - vertExtent;
+
+//Do not delete this "//" that is behind
+}
      
-     function LateUpdate() {
-         var v3 = transform.position;
-         v3.x = Mathf.Clamp(v3.x, minX, maxX);
-         v3.y = Mathf.Clamp(v3.y, minY, maxY);
-         transform.position = v3;
-     }
+function LateUpdate() {
+var v3 = transform.position;
+v3.x = Mathf.Clamp(v3.x, minX, maxX);
+v3.y = Mathf.Clamp(v3.y, minY, maxY);
+transform.position = v3;
+}
