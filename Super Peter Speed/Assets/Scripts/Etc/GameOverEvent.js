8005820746de98e2667769b5cdecb7389dfbb1e1
penguin21 <pepesanchezjose68@gmail.com>
2015-04-30 17:32:32 +0200

    Files

--- Super Peter Speed/Assets/Scripts/Etc/GameOverEvent.js
+++ Super Peter Speed/Assets/Scripts/Etc/GameOverEvent.js
@@ -1,5 +1,8 @@
 ﻿var SoundHit : AudioClip;
 var Sec = 5.0;
+var SmokePar : GameObject;
+var PointSmoke : Transform;
+private var h = false;
 
 function Start () {
 	GameOver();
@@ -10,5 +13,10 @@ function Update () {
 }
 
 function GameOver(){
-	
+	yield WaitForSeconds(Sec);
+	GetComponent(AudioSource).PlayOneShot(SoundHit);
+	if(h == false){
+		Instantiate (SmokePar, PointSmoke.transform.position, PointSmoke.transform.rotation);
+		h = true;
+	}
 }
