ac95635caf83ce75cef0e70fa180ac30f998cf9f
penguin21 <pepesanchezjose68@gmail.com>
2015-05-09 15:02:57 +0200

    Files
    Improved Enemys and add Screen Wrap

---
+++ Super Peter Speed/Assets/Scripts/Etc/ScreenWrap.js
@@ -0,0 +1,14 @@
+﻿var WarpPoint : Transform;
+
+private var IsUsing = false;
+
+function OnTriggerEnter2D(Rigid : Collider2D){
+	if(!IsUsing){
+	Rigid.transform.position.x = WarpPoint.transform.position.x;
+	WarpPoint.gameObject.GetComponent(ScreenWrap).IsUsing = true;
+	}
+}
+
+function OnTriggerExit2D(Rigid : Collider2D){
+	IsUsing = false;
+}
