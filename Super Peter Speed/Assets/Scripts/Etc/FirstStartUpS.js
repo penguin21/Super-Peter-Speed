fd5df59cc1963b3e4d165f2f9dc98ab75dd4f912
penguin21 <pepesanchezjose68@gmail.com>
2015-05-14 17:53:29 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/FirstStartUpS.js
@@ -0,0 +1,15 @@
+﻿var IsF = 0;
+
+IsF = PlayerPrefs.GetInt("StartBootF");
+
+function Start () {
+
+}
+
+function Update () {
+	if(IsF == 0){
+		PlayerPrefs.SetFloat("SoundVolume", 1);
+		PlayerPrefs.SetFloat("MusicVolume", 0.5);
+		PlayerPrefs.SetInt("StartBootF", 1);
+	}
+}
