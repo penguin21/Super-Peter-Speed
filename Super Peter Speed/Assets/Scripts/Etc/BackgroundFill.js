5f19778e89cbe0c4713784e880ab549fa15d0d40
penguin21 <pepesanchezjose68@gmail.com>
2015-05-20 16:47:47 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/Etc/BackgroundFill.js
@@ -0,0 +1,20 @@
+﻿ 
+ function Update(){
+ 	ResizeSpriteToScreen();
+ }
+ 
+ function ResizeSpriteToScreen() {
+     var sr = GetComponent(SpriteRenderer);
+     if (sr == null) return;
+     
+     transform.localScale = Vector3(1,1,1);
+     
+     var width = sr.sprite.bounds.size.x;
+     var height = sr.sprite.bounds.size.y;
+     
+     var worldScreenHeight = Camera.main.orthographicSize * 2.0;
+     var worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+     
+     transform.localScale.x = worldScreenWidth / width;
+     transform.localScale.y = worldScreenHeight / height;
+ }
---
+++ Super Peter Speed/Assets/Scripts/Etc/ParallaxCam.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxCam : MonoBehaviour {
+
+	public FreeParallax parallax;
+	public bool isX = true;
+	public bool isY = false;
+	public float CamSpeed;
+	public float CamSpeedY;
+	Vector3 lastPosition = Vector3.zero;
+	Vector3 lastPositionY = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (parallax != null){
+			if(isX){
+				parallax.Speed = CamSpeed;
+			}
+			if(isY){
+				parallax.Speed = CamSpeedY;
+			}
+		}
+		if(isX){
+			CamSpeed = transform.position.x;
+		}
+
+		if(isY){
+			CamSpeedY = transform.position.y;
+		}
+
+	}
+
+	void FixedUpdate()
+	{	
+	if(isX){
+		CamSpeed = (transform.position - lastPosition).magnitude;
+			lastPosition = transform.position;
+		}
+
+	}
+}
