b1b324f5f69204e1c0dec3b44cbde68613fe5664
penguin21 <pepesanchezjose68@gmail.com>
2015-05-24 15:21:20 +0200

    Files

---
+++ Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorID.js
@@ -0,0 +1,22 @@
+﻿var ID = 0;
+var IsNPC = false;
+var NPCObj : GameObject; //Only "IsNPC = true"
+var IsPlay = false;
+
+private var h = true;
+
+function Start () {
+
+}
+
+function Update () {
+	if(IsNPC){
+		if(IsPlay){
+			if(h){
+				h = false;
+				Instantiate (NPCObj, gameObject.transform.position, gameObject.transform.rotation);
+				Destroy(gameObject);
+			}
+		}
+	}
+}
--- Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorManager.js
+++ Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorManager.js
@@ -1,11 +1,38 @@
-﻿var LevelT : Transform;
+﻿import System.Xml;
+import System.Xml.Serialization;
+
+var LevelT : Transform;
 var IsEditor = true;
 var MenuGUI : GameObject;
+var LevelUsed = "Level.xml";
+var LevelsFolder : String = "/Levels";
+var MusicFolder : String = "/Music";
+var CustomSpritesFolder : String = "/CustomSprites";
+var LocalFolder : String = "/Super Peter Speed";
+
+private var appdata : String;
+appdata = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)+ LocalFolder;
 
 private var menu = false;
 
 function Start () {
-
+	if(!Directory.Exists(appdata + MusicFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + MusicFolder);
+     Debug.Log("Directory Music is Created on " + appdata + MusicFolder);
+ 	}
+ 	
+ 	if(!Directory.Exists(appdata + LevelsFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + LevelsFolder);
+     Debug.Log("Directory Levels is Created on " + appdata + LevelsFolder);
+ 	}
+ 	
+ 	 if(!Directory.Exists(appdata + CustomSpritesFolder)){    
+     //if it doesn't, create it
+     Directory.CreateDirectory(appdata + CustomSpritesFolder);
+     Debug.Log("Directory Custom Sprites is created on "+ appdata + CustomSpritesFolder);
+ 	}
 }
 
 function Update () {
@@ -28,4 +55,23 @@ function PanelNew (Panel : GameObject){
 
 function PanelOld(Panel : GameObject){
 	Panel.SetActive(false);
+}
+
+@XmlRoot("LevelObjects")
+public class LevelObjects{
+	@XmlArray("LevelObjects")
+ 	@XmlArrayItem("LevelObject")
+ 	public var LevelObjects : List.<LevelEditorID>;
+ 	
+	public function Save(){
+		//Not implemented
+	}
+
+	public static function Loading():LevelObjects{
+		//Not implemented
+	}
+	
+	public static function LoadFormText(text : String):LevelObjects{
+		//Not implemented
+	}
 }
