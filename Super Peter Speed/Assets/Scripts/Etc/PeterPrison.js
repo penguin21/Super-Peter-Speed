7578708bc7abc42d73acf41072e161b447ab8851
penguin21 <pepesanchezjose68@gmail.com>
2015-06-16 17:48:38 +0200

    Files
    New extension DOTween

--- Super Peter Speed/Assets/Scripts/Etc/PeterPrison.js
+++ Super Peter Speed/Assets/Scripts/Etc/PeterPrison.js
@@ -1,6 +1,9 @@
 ﻿var Sprite1 : Sprite;
 var Sprite2 : Sprite;
+var particle : GameObject;
+var SoundOnCol : AudioClip;
 
+private var h = true;
 function Start () {
 
 }
@@ -10,8 +13,17 @@ function Update () {
 }
 
 function OnTriggerEnter2D(other: Collider2D) {
+	if(h){
 	Debug.Log("Is Enter");
 	other.GetComponent.<Collider2D>().enabled = false;
+	Instantiate(particle,gameObject.transform.position,gameObject.transform.rotation);
+	GetComponent.<AudioSource>().clip = SoundOnCol;
+	GetComponent.<AudioSource>().Play();
+	h = false;
 	yield WaitForSeconds(5);
 	gameObject.GetComponent(SpriteRenderer).sprite = Sprite1;
+	yield WaitForSeconds(0.5);
+	gameObject.GetComponent(SpriteRenderer).sprite = Sprite2;
+	Debug.Log("Is Awake");
+	}
 }
