d1889b059bb0f545b9e28613a76ad44a7f9441f0
penguin21 <pepesanchezjose68@gmail.com>
2015-07-01 17:58:01 +0200

    Files

--- Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorID.js
+++ Super Peter Speed/Assets/Scripts/LevelEditor/LevelEditorID.js
@@ -1,21 +1,27 @@
-﻿var ID = 0;
-var IsNPC = false;
-var NPCObj : GameObject; //Only "IsNPC = true"
-var IsPlay = false;
+﻿import System.Collections.Generic;
+import System.Xml.Serialization;
 
-private var h = true;
+public class LevelEditorID extends MonoBehaviour{
+	@XmlAttribute("LevelObjID")
+	public var ID = 0;
+	public var IsNPC = false;
+	private var NPCObj : GameObject; //Only "IsNPC = true"
+	private var IsPlay = false;
 
-function Start () {
+	private var h = true;
 
-}
+	function Start () {
 
-function Update () {
-	if(IsNPC){
-		if(IsPlay){
-			if(h){
-				h = false;
-				Instantiate (NPCObj, gameObject.transform.position, gameObject.transform.rotation);
-				Destroy(gameObject);
+	}
+
+	function Update () {
+		if(IsNPC){
+			if(IsPlay){
+				if(h){
+					h = false;
+					Instantiate (NPCObj, gameObject.transform.position, gameObject.transform.rotation);
+					Destroy(gameObject);
+				}
 			}
 		}
 	}
--- Super Peter Speed/Assets/Scripts/LevelEditor/LevelObjects.js
+++ Super Peter Speed/Assets/Scripts/LevelEditor/LevelObjects.js
@@ -4,53 +4,56 @@ import System.IO;
 import System.Xml.Serialization;
 import System.Text;
 
-@XmlRoot("LevelObjectsCollection")
+@XmlRoot("LevelHY")
 public class LevelObjects extends MonoBehaviour{
-	@XmlArray("LevelObjects")
+	@XmlArray("LevelObjData")
  	@XmlArrayItem("LevelObject")
- 	public var LevelObjectsList : LevelEditorID[];
+ 	@XmlElement
+ 	public var LevelObjectsList : List.<LevelEditorID>;
  	public var LevelUsed = "Level";
  	public var path : String;
  	public var pathSps : String = "/Super Peter Speed";
- 	public var LevelT : GameObject;
+ 	@XmlElement
+ 	public var LevelT : Transform;
  	private var appdata : String;
  	private var LevelsFolder : String = "/Levels";
  	
  	function Update(){
  		appdata = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + pathSps;
  		path = appdata + LevelsFolder;
-		LevelObjectsList = GameObject.FindObjectsOfType(LevelEditorID);
  	}
  	
  	public function Save(path : String){
 		//Not implemented
-		var serializer : XmlSerializer = new XmlSerializer(LevelObjects);
+		var serializer : XmlSerializer = new XmlSerializer(LevelEditorID);
  		var stream : Stream = new FileStream(path, FileMode.Create);
  		serializer.Serialize(stream, this);
+ 		Debug.Log("Level Saving...");
  		stream.Close();
  		Debug.Log("Level Saved");
 	}
 
-	public static function Loading(path : String):LevelObjects{
+	public static function Loading(path : String):LevelEditorID{
 		//Not implemented
-		var serializer : XmlSerializer = new XmlSerializer(LevelObjects);
+		var serializer : XmlSerializer = new XmlSerializer(LevelEditorID);
  		var stream : Stream = new FileStream(path, FileMode.Open);
- 		var level : LevelObjects = serializer.Deserialize(stream) as LevelObjects;
+ 		var level : LevelEditorID = serializer.Deserialize(stream) as LevelEditorID;
+ 		Debug.Log("Level Loading...");
  		stream.Close();
  		Debug.Log("Level Loaded");
  		return level;
 	}
 	
-	public static function LoadFormText(text : String):LevelObjects{
+	public static function LoadFormText(text : String):LevelEditorID{
 		//Not implemented
-		var serializer : XmlSerializer = new XmlSerializer(LevelObjects);
- 		return serializer.Deserialize(new StringReader(text)) as LevelObjects;
+		var serializer : XmlSerializer = new XmlSerializer(LevelEditorID);
+ 		return serializer.Deserialize(new StringReader(text)) as LevelEditorID;
 	}
 	public function Write(){
 		Save(Path.Combine(path, LevelUsed));
 	}
 
 	public function Read(){
-		var LevelData : LevelObjects = LevelObjects.Loading(Path.Combine(path, LevelUsed));
+		var LevelData : LevelEditorID = LevelEditorID.Loading(Path.Combine(path, LevelUsed));
 	}
 }
